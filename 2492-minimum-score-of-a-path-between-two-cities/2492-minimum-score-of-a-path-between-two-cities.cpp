4d935cf87492c5de2ff0b432192c6008888c13a4
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-03-22 20:12:26 +0530

    Time: 430 ms (92.92%), Space: 123.7 MB (81.39%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    void dfs(int src, vector<pair<int, int>> adj[], vector<int> &vis)
+    void dfs(int src, vector<pair<int, int>> adj[],int &min_distance, vector<int> &vis)
     {
         vis[src] = 1;
         for (auto &it : adj[src])
         {
             int child = it.first, dis = it.second;
-            if (vis[child])
-                continue;
-            
-            dfs(child, adj, vis);
+             min_distance = min(min_distance,dis);
+            if (!vis[child])
+                  dfs(child, adj, min_distance, vis);
         }
     }
     
@@ -26,14 +25,7 @@ public:
         }
         
         vector<int> vis(n + 1, 0);
-        dfs(1, adj, vis);
-
-        for (int i = 0; i < m; i++)
-        {
-            int a = roads[i][0], b = roads[i][1], dis = roads[i][2];
-            if (vis[a] && vis[b])
-                min_distance = min(min_distance, dis);
-        }
+        dfs(1, adj,min_distance, vis);
         
         return min_distance;
     }
