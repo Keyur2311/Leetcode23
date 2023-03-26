e5e013b69eb849099ee2fdd3dc89b3e60a2f024a
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-03-26 14:19:16 +0530

    Update 0684-redundant-connection.cpp

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,3 +1,4 @@
+// APPROCH 1:- USING DFS CYCLE DETECTION
 class Solution
 {
 public:
@@ -50,5 +51,32 @@ public:
         }
 
         return res;
+    }
+};
+
+// APPROACH 2:- USING UNION FIND
+class DSU {
+    vector<int> par, rank;
+public:
+    DSU(int n) : par(n), rank(n) {
+        iota(begin(par), end(par), 0);
+    }
+    int find(int x) {
+        if(x == par[x]) return x;           // x is itself the parent of this component
+        return par[x] = find(par[x]);       // update parent of x before returning for each call
+    }
+    bool Union(int x, int y) {
+        auto xp = find(x), yp = find(y);    // find parents of x and y, i.e, representatives of components that x and y belong to
+        if(xp == yp) return false;          // x and y already belong to same component - not possible to union
+        return par[xp] = yp;                // union x and y by making parent common
+    }
+};
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& e) {
+        DSU ds(size(e)+1);
+        for(auto& E : e) 
+            if(!ds.Union(E[0], E[1])) return E;	// not possible to union - adding this edge was causing the cycle
+        return { };    // un-reachable
     }
 };
