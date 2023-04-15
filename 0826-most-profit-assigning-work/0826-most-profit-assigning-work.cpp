06cde80260667394153247c9668150318cb8e0a8
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-04-15 17:51:56 +0530

    Time: 75 ms (78.12%), Space: 36.2 MB (94.64%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -0,0 +1,55 @@
+class Solution{
+public:
+    // class Compare {
+    // public:
+    //     bool operator()(const pair<int, int> &a,const pair<int, int> &b){
+    //         if (a.first < b.first)
+    //             return true;
+    //         else if (a.first == b.first && a.second < b.second)
+    //             return true;
+    //         else
+    //             return false;
+    //     }
+    // };
+    
+    int maxProfitAssignment(vector<int> &difficulty, vector<int> &profit, vector<int> &worker){
+        int n = (int)profit.size();
+        int m = (int)worker.size();
+        
+         auto cmp = [&](const pair<int, int> &a, const pair<int, int> &b){
+            if (a.first < b.first)
+                return true;
+            else if (a.first == b.first && a.second > b.second)
+                return true;
+            else
+                return false;
+        };
+
+        vector<pair<int, int>> vp(n);
+        for (int i = 0; i < n; i++)
+            vp[i] = {difficulty[i], profit[i]};
+
+        sort(vp.begin(), vp.end(), cmp);
+
+        for (int i = 1; i < n; i++)
+            vp[i].second = max(vp[i].second, vp[i - 1].second);
+
+        int ans = 0;
+
+        for (int i = 0; i < m; i++){
+            if (worker[i] < vp[0].first)
+                continue;
+            int x = worker[i];
+            if (x > vp[n - 1].first)
+                x = vp[n - 1].first;
+            
+            pair<int, int> p = {x, INT_MIN};
+            auto it = lower_bound(vp.begin(), vp.end(), p) - vp.begin();
+            if (vp[it].first > worker[i])
+                it--;
+            ans += vp[it].second;
+        }
+
+        return ans;
+    }
+};
