bb04bc5ebbf540b17b1511c467bc2d411e3c8d9b
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-07-24 21:45:32 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (28.53%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x,n);
+  double myPow(double x, int n) {
+
+    if (n < 0)
+      x = 1 / x;
+
+    double pow = 1;
+    long long int num = labs(n);
+
+    while (num) {
+      if (num & 1) {
+        pow *= x;
+      }
+      x *= x;
+      num >>= 1;
     }
+
+    return pow;
+  }
 };
