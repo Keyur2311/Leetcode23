a1973ac39192c4de419d28dddf3ce5e328b7f36c
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-08-02 00:27:47 +0530

    Time: 28 ms (43.55%), Space: 29.9 MB (15.43%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.cpp
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.cpp
@@ -1,39 +1,23 @@
 class Solution {
-public:
-  int minDays(int n) {
-
-    if (n == 1)
-      return 1;
-
-    queue<int> q;
-    q.push(n);
-
-    set<int> st;
-    int ans = 0;
-
-    while (!q.empty()) {
-      ans++;
-
-      int sz = q.size();
-      while (sz--) {
-        int tp = q.front();
-        q.pop();
-
-        if (tp == 0)
-          return ans - 1;
-        if (st.count(tp))
-          continue;
-
-        st.insert(tp);
-
-        q.push(tp - 1);
-        if (tp % 2 == 0)
-          q.push(tp / 2);
-        if (tp % 3 == 0)
-          q.push(tp / 3);
-      }
+public: 
+    int inner(int n) {
+        if (dp.find(n) != dp.end()) {
+            return dp[n];
+        }
+        int result = INT_MAX;
+		
+        if (n % 3 == 0) result = min(result, inner(n/3));
+        if (n % 2 == 0) result = min(result, inner(n/2));
+		
+        if (n % 3 != 0 || n% 2 != 0) result = min(result, inner(n-1));
+        dp[n] = result + 1;
+        return dp[n];
     }
-
-    return ans;
-  }
+public: 
+    unordered_map<int, int> dp;
+    int minDays(int n) {
+        dp[0] = 0;
+        dp[1] = 1;
+        return inner(n);
+    }
 };
