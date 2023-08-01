534cf0d07bcdb612bb6ec77151501e80416aa6dc
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-08-01 22:41:23 +0530

    Time: 21 ms (99.95%), Space: 18.9 MB (37.58%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int hr) {
-        auto ok = [&](int val)
-    {
-        long long ctr = 0;
-        for (auto &x : piles)
-            ctr += (long long)((x + val - 1) / val);
-        return (ctr <= hr);
+  int minEatingSpeed(vector<int> &piles, int hr) {
+    auto ok = [&](int val) {
+      long long ctr = 0;
+      for (auto &x : piles)
+        ctr += (long long)((x + val - 1) / val);
+      return (ctr <= hr);
     };
 
     long long l = 1;
     long long r = 1e9;
     long long mid, ans = 1e9;
 
-    while (l <= r)
-    {
-        mid = l + (r - l) / 2;
-        if (ok(mid))
-        {
-            ans = min(ans, mid);
-            r = mid - 1;
-        }
-        else
-            l = mid + 1;
+    while (l <= r) {
+      mid = l + (r - l) / 2;
+      if (ok(mid)) {
+        ans = min(ans, mid);
+        r = mid - 1;
+      } else
+        l = mid + 1;
     }
 
     return ans;
-    }
+  }
 };
-
-
