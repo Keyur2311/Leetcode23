fe8b8aaa84da9e60b2db9e89d87943daeef1526a
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-08-12 07:12:59 +0530

    Time: 4 ms (52.97%), Space: 7.6 MB (81.25%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -3,24 +3,30 @@ public:
   int uniquePathsWithObstacles(vector<vector<int>> &grid) {
     int n = grid.size();
     int m = grid[0].size();
-      
-    if(grid[0][0]==1) return 0;
+
+    if (grid[0][0] == 1)
+      return 0;
 
     int dp[n][m];
     dp[0][0] = 1;
+      
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         if (grid[i][j] == 1)
           dp[i][j] = 0;
         else {
-            if(i==0 && j==0) continue;
-            if(i==0) dp[i][j] = dp[i][j-1];
-            else if(j==0) dp[i][j] = dp[i-1][j];
-           else
-               dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+          if (i == 0 && j == 0)
+            continue;
+          if (i == 0)
+            dp[i][j] = dp[i][j - 1];
+          else if (j == 0)
+            dp[i][j] = dp[i - 1][j];
+          else
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
         }
       }
     }
+      
     return dp[n - 1][m - 1];
   }
 };
