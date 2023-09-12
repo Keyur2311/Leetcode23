ba05e647d3ecefa9e71b9bdb40969ff563dc49bc
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-09-12 23:47:25 +0530

    Time: 9 ms (47.38%), Space: 21.3 MB (56.30%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -11,26 +11,27 @@
  */
 class Solution {
 public:
-    bool ok = false;
-    bool isLeaf(TreeNode * root){ 
-    return !root->left && !root->right;
+  bool ok = false;
+  bool isLeaf(TreeNode *root) { return !root->left && !root->right; }
+  void pathsum(TreeNode *root, int sum, int targetSum) {
+
+    if (isLeaf(root)) {
+      sum += root->val;
+      if (sum == targetSum) {
+        ok = true;
+        return;
+      }
     }
-    void pathsum(TreeNode * root,int sum,int targetSum){
-        
-        if(isLeaf(root)){
-            sum+=root->val;
-            if(sum==targetSum) {
-                ok=true;
-                return;
-            }
-        }
-        sum+=root->val;
-        if(root->left) pathsum(root->left,sum,targetSum);
-        if(root->right) pathsum(root->right,sum,targetSum);
-    }
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        if(root==NULL) return false;
-        pathsum(root,0,targetSum);
-        return ok;
-    }
+    sum += root->val;
+    if (root->left)
+      pathsum(root->left, sum, targetSum);
+    if (root->right)
+      pathsum(root->right, sum, targetSum);
+  }
+  bool hasPathSum(TreeNode *root, int targetSum) {
+    if (root == NULL)
+      return false;
+    pathsum(root, 0, targetSum);
+    return ok;
+  }
 };
