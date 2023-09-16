7388b5a320e62c8335c8f0cccb9c23212a0d1952
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-09-16 11:31:12 +0530

    Time: 33 ms (51.26%), Space: 35.1 MB (6.30%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
   TreeNode *solve(TreeNode *root1, TreeNode *root2) {
@@ -21,22 +10,23 @@ public:
     else if (root2 != NULL) {
       newRoot = new TreeNode(root2->val);
     }
-    
-    if(newRoot!=NULL){
-    if (root1 != NULL && root2 != NULL) {
-      newRoot->left = solve(root1->left, root2->left);
-      newRoot->right = solve(root1->right, root2->right);
-    } else if (root2 != NULL) {
-      newRoot->left = solve(root1, root2->left);
-      newRoot->right = solve(root1, root2->right);
-    } else if (root1 != NULL) {
-      newRoot->left = solve(root1->left, root2);
-      newRoot->right = solve(root1->right, root2);
-    } else {
-      newRoot->left = NULL;
-      newRoot->right = NULL;
+
+    if (newRoot != NULL) {
+      if (root1 != NULL && root2 != NULL) {
+        newRoot->left = solve(root1->left, root2->left);
+        newRoot->right = solve(root1->right, root2->right);
+      } else if (root2 != NULL) {
+        newRoot->left = solve(root1, root2->left);
+        newRoot->right = solve(root1, root2->right);
+      } else if (root1 != NULL) {
+        newRoot->left = solve(root1->left, root2);
+        newRoot->right = solve(root1->right, root2);
+      } else {
+        newRoot->left = NULL;
+        newRoot->right = NULL;
+      }
     }
-    }
+
     return newRoot;
   }
   TreeNode *mergeTrees(TreeNode *root1, TreeNode *root2) {
