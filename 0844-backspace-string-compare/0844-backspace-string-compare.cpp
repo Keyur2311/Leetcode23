804dcad779886ee5e816d7702a8d16776292c945
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-10-19 21:10:08 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (69.01%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -3,26 +3,45 @@ public:
   string solve(string st) {
     int n = (int)st.size();
     int ctr = 0;
-    string t = "";
     for (int i = n - 1; i >= 0; i--) {
       if (st[i] == '#')
         ctr++;
       else {
         if (ctr > 0) {
+          st[i] = '*';
           ctr--;
           continue;
-        } else
-          t.push_back(st[i]);
+        }
       }
     }
 
-    reverse(t.begin(), t.end());
-    return t;
+    return st;
   }
   bool backspaceCompare(string s, string t) {
     string s1 = solve(s);
     string s2 = solve(t);
+    int n = (int)s1.size(), m = (int)s2.size();
 
-    return (bool)(s1 == s2);
+    int i = 0, j = 0;
+    while (true) {
+
+      while (i < n && (s1[i] == '*' || s1[i] == '#')) {
+        i++;
+        continue;
+      }
+      while (j < m && (s2[j] == '*' || s2[j] == '#')) {
+        j++;
+        continue;
+      }
+
+      if (i < n && j < m && s1[i] != s2[j])
+        return false;
+
+      if (i >= n || j >= m)
+        break;
+      i++, j++;
+    }
+
+    return (bool)(i == n && j == m);
   }
 };
