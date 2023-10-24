60a2391762628d9a599b8b9dfa8a5bcb72c1c2c4
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-10-24 23:05:54 +0530

    Added solution - LeetHub

---
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial Template for c++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+  bool isPalindrome(int left, int right, string &s) {
+    while (left < right) {
+      if (s[left] != s[right])
+        return false;
+      left++;
+      right--;
+    }
+    return true;
+  }
+  int solve(string s, int left, int right, vector<int> &dp) {
+    int sz = (int)s.size();
+
+    if (right == sz - 1)
+      return 0;
+    if (dp[right] != -1)
+      return dp[right];
+
+    int op = 0;
+    int temp = INT_MAX - 1;
+    for (int i = sz - 1; i > right; i--) {
+
+      if (isPalindrome(right + 1, i, s)) {
+        temp = min(temp, 1 + solve(s, right + 1, i, dp));
+      }
+    }
+    op = temp;
+    return dp[right] = op;
+  }
+
+  int palindromicPartition(string s) {
+    int sz = (int)s.size();
+    vector<int> dp(sz + 1, -1);
+    int ans = INT_MAX;
+
+    for (int i = sz - 1; i >= 0; i--) {
+      if (isPalindrome(0, i, s))
+        ans = min(ans, solve(s, 0, i, dp));
+    }
+    return ans;
+  }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.palindromicPartition(str)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
