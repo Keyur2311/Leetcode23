3dc6daf2be8e74c63b4ccaae3cde39cb1797ea48
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-10-27 23:03:40 +0530

    Time: 271 ms (34.01%), Space: 290 MB (12.31%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,40 +1,44 @@
-class Solution {
+class Solution
+{
 public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        
-        bool dp[n+1][n+1];
-        int length = 0,start=0;
-       for(int gap=0;gap<n;gap++){
-           for(int i=0,j=gap;j<n;i++,j++){
-               if(gap==0)
-                   dp[i][j]=true;
-               else if(gap==1)
-               {
-                   if(s[i]==s[j])
-                       dp[i][j]=true;
-                   else
-                       dp[i][j]=false;
-               }else{
-                   if(s[i]==s[j] && dp[i+1][j-1])
-                       dp[i][j]=true;
-                   else
-                       dp[i][j]=false;
-               }
-               
-               if(dp[i][j]){
-               if(gap+1 > length){
-                   length = gap+1;
-                   start = i;
-               }
-               }
-           }
-       }
-        
-        string ans="";
+    string longestPalindrome(string s)
+    {
+        int n = (int)s.size();
+         vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        int length = 0, start = 0;
+        for (int gap = 0; gap < n; gap++)
+        {
+            for (int i = 0, j = gap; j < n; i++, j++)
+            {
+                if (gap == 0)
+                    dp[i][j] = 1;
+                else if (gap == 1)
+                {
+                    if (s[i] == s[j])
+                        dp[i][j] = 1;
+                }
+                else
+                {
+                    if (s[i] == s[j] && dp[i + 1][j - 1])
+                        dp[i][j] = 1;
+                }
+
+                if (dp[i][j])
+                {
+                    if (gap + 1 > length)
+                    {
+                        length = gap + 1;
+                        start = i;
+                    }
+                }
+            }
+        }
+
+        string ans = "";
         int i = length;
-        while(i--){
-            ans+=s[start];
+        while (i--)
+        {
+            ans += s[start];
             start++;
         }
         return ans;
