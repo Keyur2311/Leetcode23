16ada3eaf96771f6a671bb39c7aecab356041333
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-10-29 09:45:04 +0530

    Time: 99 ms (100.00%), Space: 97.5 MB (50.00%) - LeetHub

---
+++ 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+  int n;
+  vector<long long> dp;
+  long long solve(int ind, vector<int> &nums, int k) {
+    if (ind > n - 3)
+      return 0;
+    if (dp[ind] != -1)
+      return dp[ind];
+
+    long long op1 = max(0, k - nums[ind]) + solve(ind + 1, nums, k);
+    long long op2 = max(0, k - nums[ind + 1]) + solve(ind + 2, nums, k);
+    long long op3 = max(0, k - nums[ind + 2]) + solve(ind + 3, nums, k);
+
+    return dp[ind] = min(op1, min(op2, op3));
+  }
+  long long minIncrementOperations(vector<int> &nums, int k) {
+    n = nums.size();
+    dp.resize(n, -1);
+    return solve(0, nums, k);
+  }
+};
