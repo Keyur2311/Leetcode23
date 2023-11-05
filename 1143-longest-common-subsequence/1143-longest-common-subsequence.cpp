db5406232f7d900b6a6aa37ed96a355227d7f4ef
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-11-05 13:51:52 +0530

    Time: 42 ms (74.27%), Space: 24.8 MB (48.70%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,33 +1,27 @@
-
-class Solution
-{
+class Solution {
 public:
-    int LCS(string x, string y, int n, int m)
-    {
+  int LCS(string x, string y, int n, int m) {
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = 0;
-        for (int i = 0; i <= m; i++)
-            dp[0][i] = 0;
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+    for (int i = 0; i <= n; i++)
+      dp[i][0] = 0;
+    for (int i = 0; i <= m; i++)
+      dp[0][i] = 0;
 
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                if (x[i - 1] == y[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-
-        return dp[n][m];
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+        if (x[i - 1] == y[j - 1])
+          dp[i][j] = 1 + dp[i - 1][j - 1];
+        else
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
     }
-    int longestCommonSubsequence(string text1, string text2)
-    {
-        int n = (int)text1.size();
-        int m = (int)text2.size();
-        return LCS(text1, text2, n, m);
-    }
+
+    return dp[n][m];
+  }
+  int longestCommonSubsequence(string text1, string text2) {
+    int n = (int)text1.size();
+    int m = (int)text2.size();
+    return LCS(text1, text2, n, m);
+  }
 };
