ec946875a3305e5fc647b54cfc504caa49c3dc21
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2023-12-04 09:41:15 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (73.15%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,19 +3,16 @@ public:
   void solve(int idx, vector<int> &v, vector<int> &curr,
              vector<vector<int>> &ans) {
 
-      ans.push_back(curr);
+    ans.push_back(curr);
 
     for (int i = idx; i < (int)v.size(); i++) {
-      if (i != idx && v[i] == v[i - 1])
-        continue;
-
       curr.push_back(v[i]);
       solve(i + 1, v, curr, ans);
       curr.pop_back();
     }
   }
+
   vector<vector<int>> subsets(vector<int> &nums) {
-    sort(nums.begin(),nums.end());
     vector<vector<int>> ans;
     vector<int> curr;
     solve(0, nums, curr, ans);
