6291bfd36c7aecdd531dd90c2903da08b905cf48
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-01-09 19:17:16 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (49.18%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -9,27 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution{
+
+class Solution {
 public:
-    bool isLeaf(TreeNode *root) {
-        return !root->left && !root->right;
-    }
-    
-    void getLeafSequence(TreeNode *root, vector<int> &v){
-        if (root != NULL)
-        {
-            getLeafSequence(root->left,v);
-            if (isLeaf(root))
-                v.push_back(root->val);
-            getLeafSequence(root->right,v);
-        }
-    }
+  bool isLeaf(TreeNode *root) { return !root->left && !root->right; }
 
-    bool leafSimilar(TreeNode *root1, TreeNode *root2){
-        vector<int> a, b;
-        getLeafSequence(root1, a);
-        getLeafSequence(root2, b);
-
-        return a == b;
+  void getLeafSequence(TreeNode *root, vector<int> &v) {
+    if (root != NULL) {
+      getLeafSequence(root->left, v);
+      if (isLeaf(root))
+        v.push_back(root->val);
+      getLeafSequence(root->right, v);
     }
+  }
+
+  bool leafSimilar(TreeNode *root1, TreeNode *root2) {
+    vector<int> a, b;
+    getLeafSequence(root1, a);
+    getLeafSequence(root2, b);
+
+    return a == b;
+  }
 };
