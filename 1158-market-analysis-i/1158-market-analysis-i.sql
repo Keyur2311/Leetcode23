ca04a7eb040ba7cb9b08c934f7656799a10daf98
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-07-28 23:11:49 +0530

    Time: 1233 ms (47.82%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,7 +1,13 @@
 /* Write your PL/SQL query statement below */
 
-select u.user_id AS buyer_id,
-       TO_CHAR(u.join_date,'YYYY-MM-DD') as join_date,
-       (SELECT count(*) from Orders where buyer_id = u.user_id AND EXTRACT(YEAR FROM order_date) = 2019) 
-       AS orders_in_2019 
-FROM Users u;
+-- select u.user_id AS buyer_id,
+--        TO_CHAR(u.join_date,'YYYY-MM-DD') as join_date,
+--        (SELECT count(*) from Orders where buyer_id = u.user_id AND EXTRACT(YEAR FROM order_date) = 2019) 
+--        AS orders_in_2019 
+-- FROM Users u;
+
+SELECT u.user_id as buyer_id, TO_CHAR(u.join_date,'YYYY-MM-DD') as join_date, count(o.order_id) as orders_in_2019
+FROM users u
+LEFT JOIN Orders o
+ON o.buyer_id=u.user_id AND EXTRACT(YEAR FROM order_date)=2019
+GROUP BY u.user_id,u.join_date
