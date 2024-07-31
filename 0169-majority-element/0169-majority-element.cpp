5e8bb6fbb9eb0f62dda7cd67dd1fab17f2f2c593
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-07-31 09:47:17 +0530

    Time: 16 ms (68.17%), Space: 27 MB (40.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
   int majorityElement(vector<int> &nums) {
-    int c = -1, votes = 0;
-    for (auto val : nums) {
-      if (votes == 0) {
-        votes = 1;
-        c = val;
-      } else {
-        if (val == c)
-          votes++;
-        else
-          votes--;
+     
+      int curr = -1,votes=0;
+      for(auto val:nums){
+          if(votes==0){
+              curr=val;
+              votes=1;
+          }else{
+              if(curr==val) votes++;
+              else votes--;
+          }
       }
-    }
-    return c;
+         return curr;
   }
+ 
 };
