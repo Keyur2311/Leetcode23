e92e922c523e46fa5d12718731fe71845dbebb53
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-08-06 03:20:22 +0530

    Time: 5 ms (50.51%), Space: 16.9 MB (18.46%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -24,18 +24,6 @@ public:
       }
       root = root->right;
     }
-    // while (root) {
-    //   if (root->left && root->right) {
-    //     TreeNode *t = root->left;
-    //     while (t->right)
-    //       t = t->right;
-    //     t->right = root->right;
-    //   }
-
-    //   if (root->left)
-    //     root->right = root->left;
-    //   root->left = NULL;
-    //   root = root->right;
-    // }
+  
   }
 };
