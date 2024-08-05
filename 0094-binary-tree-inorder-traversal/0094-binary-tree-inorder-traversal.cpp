aa4e94952115297cedef9922174bac34b04e4fa2
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-08-06 01:13:16 +0530

    Time: 4 ms (31.98%), Space: 10.1 MB (32.46%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,6 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// solution 1: iterative
+// class Solution {
+// public:
+//   vector<int> inorderTraversal(TreeNode *root) {
+//     if (!root)
+//       return {};
+
+//     vector<int> ans;
+//     stack<TreeNode *> st;
+//     st.push(root);
+//     root = root->left;
+//     while (!st.empty()) {
+//       while (root) {
+//         st.push(root);
+//         root = root->left;
+//       }
+//       root = st.top();
+//       st.pop();
+//       ans.push_back(root->val);
+//       root = root->right;
+//       while (root) {
+//         st.push(root);
+//         root = root->left;
+//       }
+//     }
+//     return ans;
+//   }
+// };
+
+// solution 2 : MORRIS INORDER
 class Solution {
 public:
   vector<int> inorderTraversal(TreeNode *root) {
@@ -16,21 +47,27 @@ public:
       return {};
 
     vector<int> ans;
-    stack<TreeNode *> st;
-    st.push(root);
-    root = root->left;
-    while (!st.empty()) {
-      while (root) {
-        st.push(root);
-        root = root->left;
-      }
-      root = st.top();
-      st.pop();
-      ans.push_back(root->val);
-      root = root->right;
-      while (root) {
-        st.push(root);
-        root = root->left;
+    TreeNode *curr = root;
+
+    while (curr) {
+      if (curr->left == NULL) {
+        ans.push_back(curr->val);
+        curr = curr->right;
+      } else {
+        TreeNode *prev = curr->left;
+
+        while (prev->right && prev->right != curr)
+          prev = prev->right;
+
+        // create thread
+        if (prev->right == NULL) {
+          prev->right = curr;
+          curr = curr->left;
+        } else {
+          prev->right = NULL;
+          ans.push_back(curr->val);
+          curr = curr->right;
+        }
       }
     }
     return ans;
