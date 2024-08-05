3c62ed3a50949135a831b569c1d1c8561af40875
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-08-06 01:17:43 +0530

    Time: 4 ms (31.96%), Space: 9.9 MB (44.84%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,22 +9,58 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// solution 1 :- iterative
+// class Solution {
+// public:
+//   vector<int> preorderTraversal(TreeNode *root) {
+//     if(!root) return {};
+      
+//     vector<int> ans;
+//     stack<TreeNode *> st;
+//     st.push(root);
+//     while (!st.empty()) {
+//       root = st.top();
+//       st.pop();
+//       ans.push_back(root->val);
+//       if (root->right)
+//         st.push(root->right);
+//       if (root->left)
+//         st.push(root->left);
+//     }
+//     return ans;
+//   }
+// };
+
+// solution 2 : MORRIS PREORDER
 class Solution {
 public:
   vector<int> preorderTraversal(TreeNode *root) {
-    if(!root) return {};
-      
+    if (!root)
+      return {};
+
     vector<int> ans;
-    stack<TreeNode *> st;
-    st.push(root);
-    while (!st.empty()) {
-      root = st.top();
-      st.pop();
-      ans.push_back(root->val);
-      if (root->right)
-        st.push(root->right);
-      if (root->left)
-        st.push(root->left);
+    TreeNode *curr = root;
+
+    while (curr) {
+      if (curr->left == NULL) {
+        ans.push_back(curr->val);
+        curr = curr->right;
+      } else {
+        TreeNode *prev = curr->left;
+
+        while (prev->right && prev->right != curr)
+          prev = prev->right;
+
+        // create thread
+        if (prev->right == NULL) {
+          prev->right = curr;
+          ans.push_back(curr->val);
+          curr = curr->left;
+        } else {
+          prev->right = NULL;
+          curr = curr->right;
+        }
+      }
     }
     return ans;
   }
