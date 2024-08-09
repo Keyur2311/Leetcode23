3d1042eaffaf2a04e983769ded427fd478029082
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-08-09 19:44:08 +0530

    Time: 10 ms (83.31%), Space: 13 MB (65.79%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,30 +2,35 @@ class Solution {
 public:
   int lowerBound(int target, vector<int> &ans) {
     int n = (int)ans.size();
-    int l = 0, r = n - 1, mid, idx = -1;
+    int l = 0, r = n - 1, mid, idx = n;
 
     while (l <= r) {
       mid = l + (r - l) / 2;
       if (ans[mid] >= target) {
         idx = mid;
         r = mid - 1;
-      } else
+      } else {
         l = mid + 1;
+      }
     }
     return idx;
   }
+
   int lengthOfLIS(vector<int> &arr) {
     int n = (int)arr.size();
+
+    if (n == 0) return 0;
 
     vector<int> ans;
     ans.push_back(arr[0]);
 
     for (int i = 1; i < n; i++) {
-      int idx = lowerBound(arr[i], ans);
-      if (idx < ans.size())
-        ans[idx] = arr[i];
-      else 
+      if (arr[i] > ans.back()) {
         ans.push_back(arr[i]);
+      } else {
+        int idx = lowerBound(arr[i], ans);
+        ans[idx] = arr[i];
+      }
     }
 
     return (int)ans.size();
