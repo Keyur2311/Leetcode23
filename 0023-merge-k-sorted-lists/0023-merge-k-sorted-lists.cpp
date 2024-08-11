d83a247daa868b1b635088275c149ee59e44384a
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-08-11 11:11:05 +0530

    Time: 11 ms (90.82%), Space: 18.4 MB (36.80%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,7 +10,6 @@
  */
 class Solution {
 public:
-
   class cmp {
   public:
     bool operator()(ListNode *a, ListNode *b) { return a->val > b->val; }
@@ -18,24 +17,24 @@ public:
 
   ListNode *mergeKLists(vector<ListNode *> &lists) {
     priority_queue<ListNode *, vector<ListNode *>, cmp> pq;
-    
+
+    for (int i = 0; i < (int)lists.size(); i++)
+       if (lists[i] != NULL)
+        pq.push(lists[i]);
+
     ListNode *dummy = new ListNode(-1);
     ListNode *tail = dummy;
 
-    for (int i = 0; i < lists.size(); i++) {
-      if (lists[i] != NULL)
-        pq.push(lists[i]);
-    }
-
     while (!pq.empty()) {
-      ListNode *temp = pq.top();
-      tail->next = temp;
-      tail = temp;
+      ListNode *curr = pq.top();
       pq.pop();
-      if (temp->next)
-        pq.push(temp->next);
-    }
 
+      tail->next = curr;
+      tail = curr;
+
+      if (curr->next)
+        pq.push(curr->next);
+    }
     return dummy->next;
   }
 };
