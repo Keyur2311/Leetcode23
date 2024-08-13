253654e7cef9fa1cf45e4b313dc835f9643d1eb4
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-08-13 09:59:35 +0530

    Time: 21 ms (73.38%), Space: 94.6 MB (67.47%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,47 +4,40 @@ public:
 
     int n1 = nums1.size();
     int n2 = nums2.size();
-    int n = n1 + n2;
 
     if (n1 > n2)
       return findMedianSortedArrays(nums2, nums1);
+     
+    int n = n1 + n2;
+    int low = 0, high = n1;
+    int left = (n1 + n2 + 1)/2;
 
-    int partition = (n + 1) / 2;
+    while (low <= high) {
+      int mid1 = low + (high - low) / 2;
+      int mid2 = left - mid1;
 
-    if (n1 == 0)
-      return n2 % 2 ? nums2[n2 / 2] : (nums2[n2 / 2] + nums2[n2 / 2 - 1]) / 2.0;
+      int l1 = INT_MIN, l2 = INT_MIN;
+      int r1 = INT_MAX, r2 = INT_MAX;
 
-    if (n2 == 0)
-      return n1 % 2 ? nums1[n1 / 2] : (nums1[n1 / 2] + nums1[n1 / 2 - 1]) / 2.0;
+      if (mid1 - 1 >= 0)
+        l1 = nums1[mid1 - 1];
+      if (mid2 - 1 >= 0)
+        l2 = nums2[mid2 - 1];
 
-    int left1 = 0;
-    int right1 = n1;
-    int cut1, cut2;
-    int l1, r1, l2, r2;
+      if (mid1 < n1)
+        r1 = nums1[mid1];
+      if (mid2 < n2)
+        r2 = nums2[mid2];
 
-    do {
-
-      cut1 = (left1 + right1) / 2;
-      cut2 = partition - cut1;
-
-      l1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
-
-      l2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
-
-      r1 = cut1 >= n1 ? INT_MAX : nums1[cut1];
-
-      r2 = cut2 >= n2 ? INT_MAX : nums2[cut2];
-
-      if (l1 <= r2 && l2 <= r1)
-        return n % 2 ? max(l1, l2) : (max(l1, l2) + min(r1, r2)) / 2.0;
+      if (l1 <= r2 && l2 <= r1) {
+        if (n % 2 == 1)
+          return max(l1, l2);
+        return (double)((max(l1, l2) + (min(r1, r2))) / 2.0);
+      } else if (l1 > r2)
+        high = mid1 - 1;
       else
-
-          if (l1 > r2)
-        right1 = cut1 - 1;
-      else
-        left1 = cut1 + 1;
-
-    } while (left1 <= right1);
+        low = mid1 + 1;
+    }
 
     return 0.0;
   }
