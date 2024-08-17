35dded361a3bf8ddadf6ad49be7e1fdd825a739b
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-08-17 20:08:31 +0530

    Time: 11 ms (80.72%), Space: 18.8 MB (43.98%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -16,33 +16,57 @@ public:
 };
 */
 
-//approach 1 : using level order traversal
+//approach 1 : using zero extra space
 class Solution {
 public:
   Node *connect(Node *root) {
     if (root == NULL)
       return NULL;
 
-    queue<Node *> q;
-    q.push(root);
-
-    while (!q.empty()) {
-      int n = q.size();
-      for (int i = 1; i <= n; i++) {
-
-        Node *curr = q.front();
-        q.pop();
-
-        if (i != n) 
-          curr->next = q.front();
-
-        if (curr->left)
-          q.push(curr->left);
-        if (curr->right)
-          q.push(curr->right);
+    Node *prev = root, *curr;
+    while (prev) {
+      curr = prev;
+      while (curr && curr->left) {
+        curr->left->next = curr->right;
+        if (curr->next)
+          curr->right->next = curr->next->left;
+        curr = curr->next;
       }
+      prev = prev->left;
     }
-
+    
     return root;
   }
 };
+ 
+//approach 2 : using level order traversal (extra space)
+// class Solution {
+// public:
+//   Node *connect(Node *root) {
+//     if (root == NULL)
+//       return NULL;
+
+//     queue<Node *> q;
+//     q.push(root);
+
+//     while (!q.empty()) {
+//       int n = q.size();
+//       for (int i = 1; i <= n; i++) {
+
+//         Node *curr = q.front();
+//         q.pop();
+
+//         if (i != n) 
+//           curr->next = q.front();
+
+//         if (curr->left)
+//           q.push(curr->left);
+//         if (curr->right)
+//           q.push(curr->right);
+//       }
+//     }
+
+//     return root;
+//   }
+// };
+
