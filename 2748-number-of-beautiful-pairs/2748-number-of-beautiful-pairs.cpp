cb718846e7291b35b940931170c98a2d50c821f9
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-08-18 20:37:20 +0530

    Time: 27 ms (81.24%), Space: 75.9 MB (6.05%) - LeetHub

--- 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.cpp
+++ 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.cpp
@@ -14,12 +14,19 @@ public:
   int countBeautifulPairs(vector<int> &nums) {
     int n = (int)nums.size();
 
+    map<int, int> frc, lsc;
     int ans = 0;
     for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        if (__gcd(nums[j] % 10, reverseNum(nums[i]) % 10) == 1)
-          ans++;
+      int fr = reverseNum(nums[i]) % 10;
+      int ls = nums[i] % 10;
+
+      for (auto it : frc) {
+        int a = it.first;
+        int b = ls;
+        ans += (__gcd(a, b) == 1 ? it.second : 0);
       }
+      frc[fr]++;
+      lsc[ls]++;
     }
 
     return ans;
