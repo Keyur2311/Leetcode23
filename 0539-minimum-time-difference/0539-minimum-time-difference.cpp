1682cd066fed97cf35a688c721ed751cc913e850
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-09-18 00:01:27 +0530

    Time: 10 ms (78.14%), Space: 17.7 MB (59.75%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -11,16 +11,15 @@ public:
     }
 
     sort(mins.begin(), mins.end());
- 
 
     int ans = INT_MAX;
-    for (int i = 0; i < n-1 ; i++) {
+    for (int i = 0; i < n - 1; i++) {
       ans = min(ans, mins[i + 1] - mins[i]);
       ans = min(ans, (1440 - mins[i + 1]) + mins[i]);
     }
-     
-      ans = min(ans, mins[n-1] - mins[0]);
-      ans = min(ans, (1440 - mins[n - 1]) + mins[0]);
+
+    ans = min(ans, mins[n - 1] - mins[0]);
+    ans = min(ans, (1440 - mins[n - 1]) + mins[0]);
 
     return ans;
   }
