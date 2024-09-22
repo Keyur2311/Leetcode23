260b3aa6dbf7206eabe27f1d8cf2e2c6c4b21401
Keyur Patel <82540200+Keyur2311@users.noreply.github.com>
2024-09-22 09:52:44 +0530

    Time: 4 ms (87.61%), Space: 12.5 MB (85.59%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,3 +1,4 @@
+// 3 approaches
 //approach 1 (nlogn and with extra space)
 // class Solution {
 // public:
@@ -16,25 +17,55 @@
 //     return ans;
 //   }
 // };
+
+//---------------------------------------------------------------
 //approach 2 :- recursion o(n)
+// class Solution {
+// public:
+//   void solve(int i, int n, vector<int> &ans) {
+//     if (i > n)
+//       return;
+
+//     int curr = i;
+//     ans.push_back(curr);
+//     for (int i = 0; i <= 9; i++) {
+//       int temp = curr * 10 + i;
+//       solve(temp, n, ans);
+//     }
+//   }
+//   vector<int> lexicalOrder(int n) {
+
+//     vector<int> ans;
+//     for (int i = 1; i <= 9; i++)
+//       solve(i, n, ans);
+//     return ans;
+//   }
+// };
+
+// -------------------------------------------
+//approach 3
 class Solution {
 public:
-  void solve(int i, int n, vector<int> &ans) {
-    if (i > n)
-      return;
-
-    int curr = i;
-    ans.push_back(curr);
-    for (int i = 0; i <= 9; i++) {
-      int temp = curr * 10 + i;
-      solve(temp, n, ans);
-    }
-  }
   vector<int> lexicalOrder(int n) {
 
     vector<int> ans;
-    for (int i = 1; i <= 9; i++)
-      solve(i, n, ans);
+
+    int curr = 1;
+    for (int i = 0; i < n; i++) {
+      ans.push_back(curr);
+
+      if (curr * 10 <= n)
+        curr *= 10;
+      else {
+        if (curr >= n)
+          curr /= 10;
+
+        curr++;
+
+        while (curr % 10 == 0)
+          curr /= 10;
+      }
+    }
     return ans;
   }
 };
